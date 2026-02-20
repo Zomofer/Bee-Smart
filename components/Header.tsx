8a4a1fc865e7bb0a98ff39e7c9ca01c7a026eda8
[Zomofer] <[ferchitogelves@gmail.com]>
2026-02-19 21:11:32 -0500

    feat:se agrego funcionalidad rowter al titulo y al perfil a mas opciones

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,34 +1,36 @@
 import { theme } from '@/constants/theme';
+import { router } from 'expo-router';
 import { Hexagon, User } from 'lucide-react-native';
 import React from 'react';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function Header() {
-  const handleProfilePress = () => {
-    console.log('Ir a perfil');
-    // router.push('/profile') si quieres navegación
+  const goHome = () => {
+    router.push('/(app)/home_page');
+  };
+
+  const goMore = () => {
+    router.push('/(app)/more');
   };
 
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.content}>
-        {/* Logo Izquierda */}
-        <View style={styles.left}>
-          <Hexagon color={theme.colors.primary} size={28} strokeWidth={2} />
-        </View>
+        <Hexagon color={theme.colors.primary} size={28} strokeWidth={2} />
 
-        {/* Título centrado */}
-        <Text style={styles.title}>Bee-Smart</Text>
+        <TouchableOpacity onPress={goHome}>
+          <Text style={styles.title}>Bee-Smart</Text>
+        </TouchableOpacity>
 
-        {/* Icono derecha */}
-        <TouchableOpacity style={styles.right} onPress={handleProfilePress}>
+        <TouchableOpacity onPress={goMore}>
           <User color={theme.colors.primary} size={26} strokeWidth={2} />
         </TouchableOpacity>
       </View>
     </SafeAreaView>
   );
 }
+
 const styles = StyleSheet.create({
   container: {
     backgroundColor: theme.colors.lightGray,
@@ -42,18 +44,9 @@ const styles = StyleSheet.create({
     paddingHorizontal: theme.spacing.md,
     paddingVertical: -5,
   },
-  left: {
-    width: 40,
-    alignItems: 'flex-start',
-  },
   title: {
-    alignSelf: 'center',
     fontSize: 15,
     fontWeight: '600',
     color: theme.colors.black,
-  },
-  right: {
-    width: 40,
-    alignItems: 'flex-end',
   },
 });
